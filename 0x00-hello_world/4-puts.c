253991b0809f4c1e601d5b9385cb6c9cfb3b26d5
amin0991 <86832.belhajji@students.itu.edu>
2023-03-17 03:17:05 +0100

    last 4 in 1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
- *  * main - entry point
- *   *
- *    * Description: print string
- *     * Return: always 0 (success)
- *      */
+* main - entry point
+*
+* Description: print string
+* Return: always 0 (success)
+*/
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
