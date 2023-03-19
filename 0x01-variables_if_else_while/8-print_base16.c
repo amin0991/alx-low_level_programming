ccd0f5052f42f6045b8b0e2c74c104b78d01387f
amin0991 <86832.belhajji@students.itu.edu>
2023-03-18 18:15:27 -0700

    8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,12 +6,15 @@
 * Return: 0
 */
 
-int main() {
+int main(void)
+{
 int i;
-for(i = 0; i < 10; i++) {
+for(i = 0; i < 10; i++)
+{
 putchar(i + '0');
 }
-for(i = 0; i < 6; i++) {
+for(i = 0; i < 6; i++)
+{
 putchar(i + 'a');
 }
 putchar('\n');
