5bfbab9abe602733dd43a2bb82ec222cebd698a1
amin0991 <86832.belhajji@students.itu.edu>
2023-03-18 17:35:38 -0700

    4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,16 +2,15 @@
 #include <ctype.h>
 
 /**
- * main - program that prints the alphabet in lowercase,
- * execpt e and q
- * You can only use the putchar 3 TIMES
- * Return: 0
- */
+* main - program that prints the alphabet in lowercase,
+* execpt e and q
+* You can only use the putchar 3 TIMES
+* Return: 0
+*/
 
 int main(void)
 {
 int dig = 'a';
-int dg = 'A';
 while (dig <= 'z')
 {
 if(dig == 'q' || dig == 'e')
