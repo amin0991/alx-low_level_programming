9aaf47a6d7a030c1acb3552b519cb84454908d94
amin0991 <86832.belhajji@students.itu.edu>
2023-03-21 20:31:39 -0700

    5 sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alpha characters
+ * @c: character to check
+ *
+ * Return: 1 if is alpha | 0 if not
+ */
+int print_sign(int n)
+{
+if(n > 0)
+return (1);
+else if(n = 0)
+return (0);
+else (n < 0)
+return (-1);
+}
