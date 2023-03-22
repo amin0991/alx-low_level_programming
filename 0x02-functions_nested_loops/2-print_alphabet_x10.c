5be6e1750cceeeafdeb9e7d363d10922528c50ea
amin0991 <86832.belhajji@students.itu.edu>
2023-03-21 19:47:05 -0700

    2 10xalphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - prints alphabets 10 times
+*
+* Return: void
+*/
+void print_alphabet(void)
+{
+char i;
+int j = 0;
+while (i != 10)
+{
+for (i = 'a' ; i <= 'z' ; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+j++;
+}
+}
