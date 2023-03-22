f1367de4bb3c53336dbf2f263503525296a7612f
amin0991 <86832.belhajji@students.itu.edu>
2023-03-21 19:58:56 -0700

    3 islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+nclude "main.h"
+
+/**
+* islower - chack if the charachter is lower case
+*
+* Return: return 1 if lowercase alse return 1 if not
+*/
+int _islower(int c)
+{
+if (c>=	97 || c<=122)
+	return (1);
+else
+	return (0);
+}
