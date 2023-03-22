68f1d6a474f766ecce1119919cf76a662b80a647
amin0991 <86832.belhajji@students.itu.edu>
2023-03-21 18:55:22 -0700

    0 _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main: main function
+ * _putchar: print our input to the screen
+ * always Return 0 as mentioned
+ **/
+int main(void)
+{
+char X[10]="_putchar\n";
+int i = 0;
+while(X[i])
+{
+_putchar(X[i]);
+i++;
+}
+return (0);
+	
+}
