49c6fe4fab0857993dfb63a6eb1aa8920d62807d
amin0991 <86832.belhajji@students.itu.edu>
2023-03-21 19:04:41 -0700

    0 _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * main: main function
+ * main: main function required by the class
  * _putchar: print our input to the screen
- * always Return 0 as mentioned
+ * Return: always Return 0 as mentioned
  **/
 int main(void)
 {
-char X[10]="_putchar\n";
+char X[10] = "_putchar\n";
 int i = 0;
-while(X[i])
+while( X[i] )
 {
-_putchar(X[i]);
+_putchar( X[i] );
 i++;
 }
 return (0);
