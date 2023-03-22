4b1ae91b256528546781b13b3636a840efb02573
amin0991 <86832.belhajji@students.itu.edu>
2023-03-21 20:10:27 -0700

    4 isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _isalpha - check if char is alpha or not
+* @c: character to check
+*
+* Return: 1 if is alphabet  0 if not
+*/
+int _isalpha(int c)
+{
+if ((c >= 97 && c <= 122) || (c >= 33 && c <= 126))
+return (0);
+else
+return (1);
+}
