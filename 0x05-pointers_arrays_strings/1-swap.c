9b259eb444d11c87f7068a13cffbb54a77b3278e
amin0991 <86832.belhajji@students.itu.edu>
2023-03-27 20:08:45 -0700

    2

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,13 +1,13 @@
 #include "main.h"
+/**
+ * swap_int - swaps to integer values
+ * @a: first integer
+ * @b: second integer
+ * Return: void
+ */
 
 void swap_int(int *a, int *b)
 {
-	/**
-	 * swap_int - swaps to integer values
-	 * @a: first integer
-	 * @b: second integer
-	 * Return: void
-	 */
 	int x;
 
 	x = *a;
