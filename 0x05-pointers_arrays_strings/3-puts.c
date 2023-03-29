b3c69107de7bf4a051a199d3df1c02a03b02064d
amin0991 <86832.belhajji@students.itu.edu>
2023-03-28 18:24:06 -0700

    3-puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - print string with a new line
+ * @strg: string to be printed
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
