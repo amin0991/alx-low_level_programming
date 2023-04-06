2eee99c94b90e459a7c8cd24035f5b8834794b5e
amin0991 <86832.belhajji@students.itu.edu>
2023-04-06 13:00:46 -0700

    first try

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+  _strchr - locate character in a string
+ * @s: string given
+ * @c: character to locate
+ * Return: pointer to first occurrence of character | NULL if not found
+ */
 char *_strchr(char *s, char c)
 {
 	unsigned int i;
